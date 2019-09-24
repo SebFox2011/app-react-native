e0882635ea0cedd7ed1e2f0a6a8cae7e5ec6a283
Seb <sebastien.philippe5@gmail.com>
2019-09-24 14:54:42 +0200

    Création d'un composant MovieItem qui va afficher un Movie dans Home

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
-import {View,Text} from 'react-native'
+import {View,Text,FlatList} from 'react-native'
+import MovieItem from "../components/MovieItem";
+
+
 
 class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies : []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({
+            movies: [
+                {title:'Titanic',year: 1988},
+                {title:'Intouchables',year: 2015},
+                {title:'StarWars',year: 1970},
+                {title:'StarTrek',year: 1975}
+            ]
+        })
+    }
+
     render() {
         return (
             <View>
                 <Text>Home</Text>
+                <FlatList
+                    data={this.state.movies}
+                    renderItem={({item}) => <MovieItem movie={item}/>}
+                />
             </View>
         );
     }
