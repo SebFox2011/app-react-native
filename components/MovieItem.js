5ec5119135ee364d985e28f5c5efa0cadd611d34
Seb <sebastien.philippe5@gmail.com>
2019-09-24 15:56:06 +0200

    Ajout d'un bouton pour ajouter un film (en dur) a la liste actuelle

--- components/MovieItem.js
+++ components/MovieItem.js
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
-import {View,Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+import Constants from "expo-constants/src/Constants";
 
 class MovieItem extends Component {
     render() {
         return (
-            <View>
-                <Text>{this.props.movie.title + this.props.movie.year}</Text>
+            <View style={styles.container}>
+                <Text>{this.props.movie.title +' ' +this.props.movie.year}</Text>
             </View>
         );
     }
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffcf78',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    text:{
+        color:'red'
+    }
+});
 export default MovieItem;
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View,Text,FlatList} from 'react-native'
+import {View,Text,FlatList,Button} from 'react-native'
 import MovieItem from "../components/MovieItem";
 
 
@@ -19,18 +19,31 @@ class Home extends Component {
                 {title:'Intouchables',year: 2015},
                 {title:'StarWars',year: 1970},
                 {title:'StarTrek',year: 1975}
+
             ]
         })
+    }
+    addFilm(){
+        const movie = {title:'Terminator 6', year:2019};
+        this.setState({
+            movies:[...this.state.movies,movie]
+        });
     }
 
     render() {
         return (
             <View>
                 <Text>Home</Text>
+
                 <FlatList
                     data={this.state.movies}
                     renderItem={({item}) => <MovieItem movie={item}/>}
                     keyExtractor={(item,index) => index.toString()}
+                />
+                <Button
+                    title="Ajouter un film"
+                    color="#000"
+                    onPress={() => this.addFilm()} // fonction fléchée pour garder le contexte
                 />
             </View>
         );
