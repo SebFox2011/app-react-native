874a8d148900558ec55041897bcfd423e07f5ac2
Seb <sebastien.philippe5@gmail.com>
2019-09-25 15:23:36 +0200

    Sur le menu stack search, ajout d'un champ de recherche qui interroge l'api par mot clé et affichage

--- screens/Search.js
+++ screens/Search.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
-import {View,Text} from "react-native";
+import {View,Text,FlatList} from "react-native";
+import TextInput from "../kitui/TextInput";
+import CompanieItem from "../components/CompanieItem";
 
 class Search extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            result:'',
+            companies:[]
+        };
+    }
+
+    searchChangeText (text){
+        fetch(process.env.API_URL+'/companies?search='+text)
+            .then(response => response.json())
+            .then(companies => this.setState({companies:companies}))
+       this.setState({result:text})
+    }
+
     render() {
         return (
             <View>
                 <Text>Search Screen</Text>
+                <TextInput value={this.state.result} placeholder={'Entreprise à rechercher'}
+                           onChangeText={text => this.searchChangeText(text)}
+                />
+                <FlatList
+                    data={this.state.companies}
+                    renderItem={({item}) => <CompanieItem companie={item}/>}
+                    keyExtractor={(item,index) => index.toString()}
+
+                />
             </View>
         );
     }
